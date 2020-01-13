f951ebc908a721b686913f0a42b4dd52df633cef
Thibence <salencethibaut95@gmail.com>
2020-01-13 11:39:10 +0100

    random commit, rn in the middle of adding empty html divs

--- script.js
+++ script.js
@@ -5,16 +5,33 @@
 
 
    submit.addEventListener("click", function () {
-      fetch('https://api.openweathermap.org/data/2.5/forecast?q='+input.value+'&appid=e1e90a3c7a628db7d39dcee70ad79160')
+      fetch('https://api.openweathermap.org/data/2.5/forecast?q='+input.value+'&units=metric&appid=e1e90a3c7a628db7d39dcee70ad79160')
 
           .then((response) => {
              return response.json()
           })
 
           .then(data => {
-              console.log(data);
+              console.log(data.list[0]);
 
-              
+
+              let avgTemp = arr => Math.floor(((arr.reduce((a,b) => a + b, 0) / arr.length)));
+
+              let tempArr = [];
+              let descriptionArr = [];
+
+              for (let i=0; i < 40; i++) {
+                  tempArr.push(data.list[i].main.temp);
+                  descriptionArr.push(data.list[i].weather[0].description);
+              }
+              //console.log(tempArr);
+              let dayOneTemp = tempArr.slice(0, 8);
+              let dayTwoTemp = tempArr.slice(0, 16);
+              let dayThreeTemp = tempArr.slice(16, 24);
+              let dayFourTemp = tempArr.slice(24, 32);
+              let dayFiveTemp = tempArr.slice(32, 40);
+
+              console.log(dayOneTemp);
 
 
           })
@@ -28,25 +45,6 @@
 
 
 
-
-
-
-
-
-/*$(document.ready(function () {
-
-    $('#submitWeather').click(function () {
-        let city = $("#city").val();
-
-        if (city != '') {
-
-        }
-        else {
-            $("#error").html("field can not be empty");
-        }
-    })
-
-}));*/
 
 /*
 1: fetch van api (kan met axios)
