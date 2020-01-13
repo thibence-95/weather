1d8a7f5b9d4ff446e0ff679f87ff33a6dff67f86
Thibaut Salence <salencethibaut95@gmail.com>
2020-01-13 08:26:52 +0100

    adding this for what it's worth

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
-$(document.ready(function () {
+(function () {
+
+   let input = document.getElementById("input");
+   let submit = document.getElementById("submitWeather");
+
+
+   submit.addEventListener("click", function () {
+      fetch(e1e90a3c7a628db7d39dcee70ad79160)
+
+          .then((response) => {
+             return response.json()
+          })
+
+          .then()
+   })
+    
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*$(document.ready(function () {
 
     $('#submitWeather').click(function () {
         let city = $("#city").val();
@@ -11,10 +42,10 @@ $(document.ready(function () {
         }
     })
 
-}))
+}));*/
 
 /*
-1fetch van api (kan met axios)
-2data filteren (json terug -> variable)
-3tonen op pagina (variable in html pompen */
+1: fetch van api (kan met axios)
+2: data filteren (json terug -> variable)
+3: tonen op pagina (variable in html pompen */
 // keep it stupid simple, stupid!!!
