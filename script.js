3e81d405384d80631f0b306ca9d86dcc4ce30333
Thibence <salencethibaut95@gmail.com>
2020-01-13 12:32:07 +0100

    got something in the divs w innerHTML

--- script.js
+++ script.js
@@ -15,8 +15,6 @@
               console.log(data.list[0]);
 
 
-              let avgTemp = arr => Math.floor(((arr.reduce((a,b) => a + b, 0) / arr.length)));
-
               let tempArr = [];
               let descriptionArr = [];
 
@@ -24,6 +22,7 @@
                   tempArr.push(data.list[i].main.temp);
                   descriptionArr.push(data.list[i].weather[0].description);
               }
+
               //console.log(tempArr);
               let dayOneTemp = tempArr.slice(0, 8);
               let dayTwoTemp = tempArr.slice(0, 16);
@@ -31,7 +30,36 @@
               let dayFourTemp = tempArr.slice(24, 32);
               let dayFiveTemp = tempArr.slice(32, 40);
 
+              let avgTemp = arr => Math.floor(((arr.reduce((a,b) => a + b, 0) / arr.length)));
+
+              let dayOneDescr = descriptionArr.slice(0, 8);
+              let dayTwoDescr = descriptionArr.slice(0, 16);
+              let dayThreeDescr = descriptionArr.slice(16, 24);
+              let dayFourDescr = descriptionArr.slice(24, 32);
+              let dayFiveDescr = descriptionArr.slice(32, 40);
+
+              let day1descHTML = document.getElementById("descriptionDay1");
+              let day2descHTML = document.getElementById("descriptionDay2");
+              let day3descHTML = document.getElementById("descriptionDay3");
+              let day4descHTML = document.getElementById("descriptionDay4");
+              let day5descHTML = document.getElementById("descriptionDay5");
+
+              let day1tempHTML = document.getElementById("temp1");
+              let day2tempHTML = document.getElementById("temp2");
+              let day3tempHTML = document.getElementById("temp3");
+              let day4tempHTML = document.getElementById("temp4");
+              let day5tempHTML = document.getElementById("temp5");
+
+              day1descHTML.innerHTML = dayOneDescr;
+              day2descHTML.innerHTML = dayTwoDescr;
+              day3descHTML.innerHTML = dayThreeDescr;
+              day4descHTML.innerHTML = dayFourDescr;
+              day5descHTML.innerHTML = dayFiveDescr;
+
+
+
               console.log(dayOneTemp);
+              console.log(dayOneDescr);
 
 
           })
